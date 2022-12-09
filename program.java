98a85070473653a9a0fe428e5a5f1a3031d6b8a9
AyratGizz <gizzatullin-airat@mail.ru>
2022-12-09 20:16:17 +0500

    star_java

--- program.java
+++ program.java
@@ -1,6 +1,13 @@
 public class program {
-    public static void main(String[] args) {
-        System.out.println("bye world");
+    public static void main(String[] args){
+        short age = 10;
+        int salary = 123456;
+        System.out.println(age);
+        System.out.println(salary);
+        // 
+        float e = 2.7f; // f - обязательно указывать суфикс
+        double pi = 3.1415; // 3.1415D - D - суфикс не обязательно прописывать
+        System.out.println(e);
+        System.out.println(pi);
     }
 }
-
