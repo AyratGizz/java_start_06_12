ad170d4c11522f048d9f5535a11008e170c932a8
AyratGizz <gizzatullin-airat@mail.ru>
2022-12-20 19:33:10 +0500

    DZ_seminar_4

---
+++ java_start_06_12/java_start_06_12/src/Seminar_4/Simplify_Path_71.java
@@ -0,0 +1,30 @@
+package Seminar_4;
+
+import java.util.Stack;
+
+public class Simplify_Path_71 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public String simplifyPath(String path) {
+            String[] dirs = path.split("/");
+            Stack<String> stack = new Stack<>();
+
+            for (String dir : dirs) {
+                if (dir.isEmpty() || dir.equals(".")) continue;
+                if (dir.equals("..")) {
+                    if (!stack.isEmpty())
+                        stack.pop();
+                } else {
+                    stack.push(dir);
+                }
+            }
+
+            return "/" + String.join("/", stack);
+        }
+    }
+
+}
